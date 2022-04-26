98469b64e1b09e13cce22cceb3e99d2e10451af9
Wroten <xionghu@sunline.cn>
2022-04-26 16:33:44 +0800

    20220426 add readme.txt

---
+++ bkanvm/readme.txt
@@ -0,0 +1,17 @@
+# The file describes the release details and change logs.
+# At least 10 valid and 10 invalid sample bank account numbers should be provided for testing purpose.
+
+# release details
+
+
+# change logs
+
+
+# valid sample bank account numbers
+
+
+# invalid sample bank account numbers
+
+
+
+
--- bkanvm/src/com/hkicl/ecp/bkanvm/b039/BankAcctNoValidator_039.java
+++ bkanvm/src/com/hkicl/ecp/bkanvm/b039/BankAcctNoValidator_039.java
@@ -4,23 +4,52 @@ import com.hkicl.ecp.bkanvm.BankAcctNoValidator;
 
 public class BankAcctNoValidator_039 implements BankAcctNoValidator {
     public int validate(String bankCode, String bankAcctNo) {
-        if (isValidBranch() && isValidAccNum()) {
+
+        boolean isValidBranch = isValidBranch();
+        boolean isValidAccNum = isValidAccNum();
+        boolean isRequireBranchCheck = isRequireBranchCheck();
+
+        if (isValidBranch && isValidAccNum) {
+            /**
+             * Valid Bank Account Number and Bypass Branch Number Checking
+             */
             return 0;
-        } else if (isRequireBranchCheck() && isValidAccNum()) {
+        } else if (isRequireBranchCheck && isValidAccNum) {
+            /**
+             * Valid Bank Account Number But Require Further Branch Number Checking
+             */
             return 1;
-        } else
+        } else {
+            /**
+             * Invalid Bank Account Number
+             */
             return 2;
+        }
     }
 
-
+    /**
+     * 行号验证
+     *
+     * @return
+     */
     private boolean isValidBranch() {
         return false;
     }
 
+    /**
+     * 帐号验证
+     *
+     * @return
+     */
     private boolean isValidAccNum() {
         return false;
     }
 
+    /**
+     * 分行号验证
+     *
+     * @return
+     */
     private boolean isRequireBranchCheck() {
         return false;
     }
